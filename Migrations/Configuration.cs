079fe62f264f6bb557f5aa3fe6eda8775b0db133
Puja Raj <puja.raj@terralogic.com>
2021-06-11 15:29:21 +0530

    my first com

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,38 @@
+namespace StudentManagement.Migrations
+{
+    using StudentManagement.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<StudentManagement.Models.StudentManagementContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(StudentManagement.Models.StudentManagementContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            context.Students.AddOrUpdate(p => p.Id,
+                new Student { Name = "Allen" },
+                new Student { Name = "Kim" },
+                new Student { Name = "Jane" }
+            );
+        }
+    }
+}
